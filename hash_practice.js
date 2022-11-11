6c3f2f0015c28da572fbff4413b2e7318e48731d
Travis Parks <travisparks0788@gmail.com>
2022-11-10 19:28:56 -0500

    Initial commit

---
+++ hash_practice.js
@@ -0,0 +1,12 @@
+person = {
+    name: "Travis",
+    location: "Chatham",
+    gender: "Male",
+    age: 34
+}
+console.log(person)
+console.log(Object.keys(person)[1])
+person.haircolor = "brown"
+person.glasses = true
+
+console.log(Object.keys(person))
